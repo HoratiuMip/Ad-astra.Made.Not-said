338b0f5b11cd98900abb149ef101fe236e0dd4e0
mipsan <97190654+HoratiuMip@users.noreply.github.com>
2024-10-08 00:07:50 +0300

    asd

--- IXT/Assembles/Blur-tool/main.cpp
+++ IXT/Assembles/Blur-tool/main.cpp
@@ -6,15 +6,33 @@
 using namespace IXT;
 
 int main( int argc, char* argv[] ) {
-    Endec::Bmp bmp{ ASSET_BMP_AHRI_PATH };
+    Surface surf{ "Blur-tool", { .0, .0 }, { std::min( Env::w<3.>(), Env::h<3.>() ) }, SURFACE_STYLE_LIQUID };
+    
+    auto render_th = std::jthread{ [ & ] () -> void {
+        Renderer2 render{ surf };
 
-    for( int row = 0; row < bmp.height; ++row ) {
-        for( int col = 0; col < bmp.width; ++col ) {
-            bmp[ row ][ 3 * col ] = 0;
-            bmp[ row ][ 3 * col + 1 ] = 40;
-            bmp[ row ][ 3 * col + 2 ] = 0;
+        RdlSweep2 sweep = { 
+            render, { .0, .0 }, { .0, .0 }, { .2, .2 }, {
+                Sweep2gcn_t{ { 10, 10, 20 }, .0 },
+                Sweep2gcn_t{ { 90, 10, 230 }, .62 },
+                Sweep2gcn_t{ { 10, 10, 20 }, .62 },
+                Sweep2gcn_t{ { 10, 10, 20 }, 1.0 }
+            }, surf.height() 
+        };
+
+        while( !surf.down( SurfKey::ESC ) ) {
+            render.charge().fill( RGBA{ .0, .0, .0, 1.0 } );
+            sweep.org_at( surf.ptr_v() );
+            render.line( Vec2{ -.5, .0 }, Vec2{ .5, 0 }, sweep );
+            render.splash();
+
+            std::this_thread::sleep_for( std::chrono::milliseconds( 10 ) );
         }
-    }
+    } };
 
-    bmp.write_back( "bmp_test.bmp" );
+    surf.on< SURFACE_EVENT_FILEDROP >( [ & ] ( std::vector< std::string > files, [[maybe_unused]]auto& ) -> void {
+
+    } );
+
+    surf.uplink( SURFACE_THREAD_THROUGH );
 }

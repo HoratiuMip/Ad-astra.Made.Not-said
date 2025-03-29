060cbf437fe00feeffeff8b9c21141ce80d3bdb0
mipsan <97190654+HoratiuMip@users.noreply.github.com>
2025-03-29 17:14:10 +0200

    dasd

--- WJP/wjp.hpp
+++ WJP/wjp.hpp
@@ -171,6 +171,7 @@ struct WJP_SEND_N_DESC {
 
 #define _WJP_ASSERT( c, r ) if( !( c ) ) return ( r );
 #define _WJP_ASSERT_INFO( c, e, r ) if( !( c ) ) { info->err = e; return ( r ); }
+#define _WJP_ASSERT_CTX_INFO( c, e, r ) if( !( c ) ) { context->info->err = e; return ( r ); }
 
 enum WJPErr_ : int {
     /* No error. */
@@ -201,7 +202,16 @@ enum WJPErr_ : int {
     WJPErr_NoEntry    = 8,
 
     /* The host forced the wake of the waiting thread. */
-    WJPErr_Forced     = 9
+    WJPErr_Forced     = 9,
+
+    /* The receive function wrap did not read the byte count requested by WJP. */
+    WJPErr_CountRecv  = 10,
+
+    /* The send function wrap did not write the byte count requested by WJP. */
+    WJPErr_CountSend  = 11,
+
+    /* The received packet has an invalid operation code. */
+    WJPErr_InvalidOp  = 12
 };
 const char* WJP_err_strs[] = {
     "WJPErr_None",
@@ -213,7 +223,10 @@ const char* WJP_err_strs[] = {
     "WJPErr_Size",
     "WJPErr_ConnReset",
     "WJPErr_NoEntry",
-    "WJPErr_Forced"
+    "WJPErr_Forced",
+    "WJPErr_CountRecv",
+    "WJPErr_CountSend",
+    "WJPErr_InvalidOp"
 };
 
 struct WJP_RESOLVE_RECV_INFO {
@@ -239,6 +252,13 @@ struct _WJP_RESOLVER {
     WJP_WAIT_BACK_INFO*   _info   = nullptr;
 };
 
+struct _WJP_RECV_CONTEXT {
+    void*                    data         = nullptr;
+    int32_t                  sz           = 0;
+    WJP_RESOLVE_RECV_INFO*   info         = nullptr;
+    int32_t                  recv_count   = 0;
+};
+
 /**
  * @brief Called by WJP when a new sequence for a head is required. Usually an interlocked increment does the job.
  * @returns The sequence.
@@ -247,18 +267,29 @@ typedef   std::function< int16_t( void ) >   WJP_SEQ_ACQ_FUNC;
 
 struct WJP_DEVICE {
     WJP_SRWRAP                    _srwrap      = {};
+    WJP_SEQ_ACQ_FUNC              _seq_acq     = nullptr;
+
     WJP_QGSTBL                    _qgstbl      = {};
     WJP_IBRSTBL                   _ibrstbl     = {};
+
     std::mutex                    _smtx        = {};
-    WJP_SEQ_ACQ_FUNC              _seq_acq     = nullptr;
+
     std::deque< _WJP_RESOLVER >   _resolvers   = {};
     std::mutex                    _resmtx      = {};
 
 
     void bind_srwrap( const WJP_SRWRAP& srwrap ) { _srwrap = srwrap; }
+    void bind_seq_acq( const WJP_SEQ_ACQ_FUNC& seq_acq ) { _seq_acq = seq_acq; }
+
     void bind_qgstbl( const WJP_QGSTBL& qgstbl ) { _qgstbl = qgstbl; }
     void bind_ibrstbl( const WJP_IBRSTBL& ibrstbl ) { _ibrstbl = ibrstbl; }
-    void bind_seq_acq( const WJP_SEQ_ACQ_FUNC& seq_acq ) { _seq_acq = seq_acq; }
+
+
+    int begin( void ) {
+        _WJP_ASSERT( _srwrap.recv != nullptr && _srwrap.send != nullptr, -1 );
+        _WJP_ASSERT( _seq_acq != nullptr, -1 );
+        return 0;
+    }
 
 
     int32_t _sizeof_send_n_descs( WJP_SEND_N_DESC* descs, int count ) {
@@ -338,133 +369,191 @@ struct WJP_DEVICE {
         return _send_n< _HAS_HEAD >( nullptr, 0, op, seq, flags, method );
     }
 
-    
-#define _WJP_RR_ASSERT__SEND( s, e ) { int _sret = ( s ); if( _sret <= 0 ) { info->err = ( _sret == 0 ) ? WJPErr_ConnReset : e; return _sret; } info->sent_count += _sret; }
-#define _WJP_RR_NAK_IF( c, r ) if( c ) { info->nakr = "WJP_NAKR_" r; goto l_gs_nak; }
-#define _WJP_RR_ASSERT_RECV( r, t, e ) { int _rret = ( r ); if( _rret <= 0 ) { info->err = ( _rret == 0 ) ? WJPErr_ConnReset : e; return _rret; } if( _rret != ( t ) ) return -1; }
-    int resolve_recv( WJP_RESOLVE_RECV_INFO* info ) {
-        _WJP_RR_ASSERT_RECV(
-            _srwrap.recv( &info->recv_head, sizeof( WJP_HEAD ), 0 ), sizeof( WJP_HEAD ), WJPErr_Recv
+
+#define _WJP_RR_ASSERT_RECV( r, t, e ) { int _rret = ( r );\
+    if( _rret < 0 ) { context->info->err = e; return _rret; }\
+    if( _rret == 0 ) { context->info->err = WJPErr_ConnReset; return 0; }\
+    context->recv_count += _rret;\
+    if( _rret != ( t ) ) { context->info->err = WJPErr_CountRecv; return -1; } }
+
+#define _WJP_RR_ASSERT__SEND( s, e ) { int _sret = ( s );\
+    if( _sret < 0 ) { context->info->err = e; return _sret; }\
+    if( _sret == 0 ) { context->info->err = WJPErr_ConnReset; return 0; }\
+    context->info->sent_count += _sret; }
+
+#define _WJP_RR_ASSERT_AGENT( a ) { int _aret = ( a ); if( _aret <= 0 ) return _aret; } 
+
+#define _WJP_RR_NAK_IF( c, r ) { if( c ) { return this->_nak( context, r ); } }
+
+    _WJP_forceinline int _resolve_ping( _WJP_RECV_CONTEXT* context ) {
+        _WJP_RR_ASSERT__SEND( 
+            _send<>( nullptr, 0, WJPOp_Ack, context->info->recv_head._dw1.seq, 0, WJPSendMethod_Direct ), 
+            WJPErr_Send 
         );
 
-        _WJP_ASSERT_INFO( info->recv_head.is_signed(), WJPErr_NotSigned, -1 );
+        return 1;
+    }
 
-        switch( info->recv_head._dw0.op ) {
+    _WJP_forceinline int _nak( _WJP_RECV_CONTEXT* context, const char* nakr ) {
+        context->info->nakr = nakr;
+        _WJP_RR_ASSERT__SEND(
+            _send<>( nakr, nakr ? strlen( nakr ) + 1 : 0, WJPOp_Nak, context->info->recv_head._dw1.seq, 0, WJPSendMethod_Direct ),
+            WJPErr_Send 
+        );
+        return 1;
+    }
+
+    _WJP_forceinline int _resolve_qgetset( _WJP_RECV_CONTEXT* context ) {
+        _WJP_RR_NAK_IF( context->info->recv_head._dw3.sz == 0, "WJP_NAKR_BAD_SIZE" );
+
+        char buffer[ context->info->recv_head._dw3.sz ];
+        _WJP_RR_ASSERT_RECV(
+            _srwrap.recv( buffer, context->info->recv_head._dw3.sz, 0 ), context->info->recv_head._dw3.sz, WJPErr_Recv
+        );
+
+        char* delim = buffer - 1; while( *++delim != '\0' ) {
+            _WJP_RR_NAK_IF( delim - buffer >= context->info->recv_head._dw3.sz - 1, "WJP_NAKR_BAD_STRING" );
+        }
+
+        WJP_QGSTBL_ENTRY* entry = _qgstbl.query( buffer );
+        _WJP_RR_NAK_IF( entry == nullptr, "WJP_NAKR_NO_ENTRY" );
+
+        if( context->info->recv_head._dw0.op == WJPOp_QSet ) goto l_qset;
+
+    l_qget:
+        if( entry->qget_func != nullptr ) {
+            char buffer[ entry->sz ];
+
+            const char* nakr = entry->qget_func( buffer );
+            _WJP_RR_NAK_IF( nakr != nullptr, nakr );
+
+            _WJP_RR_ASSERT__SEND( 
+                _send<>( buffer, entry->sz, WJPOp_Ack, context->info->recv_head._dw1.seq, 0, WJPSendMethod_Direct ),
+                WJPErr_Send
+            );
+
+        } else if( entry->src != nullptr ) {
+            _WJP_RR_ASSERT__SEND( 
+                _send<>( entry->src, entry->sz, WJPOp_Ack, context->info->recv_head._dw1.seq, 0, WJPSendMethod_Direct ),
+                WJPErr_Send
+            );
+
+        } else {
+            _WJP_RR_NAK_IF( true, "WJP_NAKR_NO_PROCEDRE" );
+        }
+        
+        return 1;
+
+    l_qset:
+        _WJP_RR_NAK_IF( entry->read_only, "WJP_NAKR_READ_ONLY" );
+
+        int32_t sz = context->info->recv_head._dw3.sz - ( delim - buffer + 1 );
+        _WJP_RR_NAK_IF( entry->sz != sz, "WJP_NAKR_BAD_SIZE" );
+
+        if( entry->qset_func != nullptr ) {
+            const char* nakr = entry->qset_func( delim + 1, sz );
+            _WJP_RR_NAK_IF( nakr != nullptr, nakr );
+
+        } else if( entry->src != nullptr ) {
+            memcpy( entry->src, delim + 1, sz );
+
+        } else {
+            _WJP_RR_NAK_IF( true, "WJP_NAKR_NO_PROCEDRE" );
+        }
+
+        _WJP_RR_ASSERT__SEND( 
+            _send<>( nullptr, 0, WJPOp_Ack, context->info->recv_head._dw1.seq, 0, WJPSendMethod_Direct ),
+            WJPErr_Send
+        );
+
+        return 1;
+    }
+
+    _WJP_forceinline int _resolve_acknak( _WJP_RECV_CONTEXT* context ) {
+        _WJP_ASSERT_CTX_INFO( !_resolvers.empty(), WJPErr_NoResolver, -1 );
+
+        _WJP_RESOLVER& resolver = _resolvers.front();
+
+        _WJP_ASSERT_CTX_INFO( context->info->recv_head._dw1.seq == resolver._seq, WJPErr_Sequence, -1 );
+
+        WJP_WAIT_BACK_INFO* wb_info = resolver._info;
+
+        if( context->info->recv_head._dw0.op == WJPOp_Nak ) {
+            //_WJP_ASSERT_INFO( info->recv_head._dw3.sz <= WJP_NAKR_MAX_SIZE, WJP_ERR_NAKR_SIZE, -1 );
+            _WJP_RR_ASSERT_RECV(
+                _srwrap.recv( wb_info->nakr, context->info->recv_head._dw3.sz, 0 ), context->info->recv_head._dw3.sz, WJPErr_Recv
+            );
+            goto l_release_resolver;
+        }
+
+        if( resolver._dst == nullptr || context->info->recv_head._dw3.sz == 0 ) goto l_release_resolver;
+
+        _WJP_ASSERT_CTX_INFO( resolver._sz >= context->info->recv_head._dw3.sz, WJPErr_Size, -1 );
+        _WJP_RR_ASSERT_RECV(
+            _srwrap.recv( resolver._dst, context->info->recv_head._dw3.sz, 0 ), context->info->recv_head._dw3.sz, WJPErr_Recv
+        );
+
+        wb_info->recv_count += context->info->recv_head._dw3.sz;
+
+    l_release_resolver:
+        wb_info->recv_count += sizeof( WJP_HEAD );
+        
+        std::unique_lock< std::mutex > lock{ _resmtx };
+        _resolvers.pop_front();
+        lock.unlock();
+        
+        wb_info->resolved.store( true, std::memory_order_release );
+    #if defined( WJP_NOTIFIABLE_ATOMICS )
+        wb_info->resolved.notify_one();
+    #endif
+
+        return 1;
+    }
+
+    _WJP_forceinline int _resolve_iburst( _WJP_RECV_CONTEXT* context ) {
+        WJP_IBRSTBL_ENTRY* entry = _ibrstbl[ context->info->recv_head._dw1.seq ];
+        _WJP_ASSERT_CTX_INFO( entry != nullptr, WJPErr_NoEntry, -1 );
+        _WJP_ASSERT_CTX_INFO( context->info->recv_head._dw3.sz <= entry->sz, WJPErr_Size, -1 );
+
+        _WJP_RR_ASSERT_RECV(
+            _srwrap.recv( entry->dst, context->info->recv_head._dw3.sz, 0 ), context->info->recv_head._dw3.sz, WJPErr_Recv
+        );
+
+        return 1;
+    }
+
+    int resolve_recv( WJP_RESOLVE_RECV_INFO* info ) {
+        _WJP_RECV_CONTEXT _context, *context = &_context;
+        context->info = info;
+
+        _WJP_RR_ASSERT_RECV(
+            _srwrap.recv( &context->info->recv_head, sizeof( WJP_HEAD ), 0 ), sizeof( WJP_HEAD ), WJPErr_Recv
+        );
+
+        _WJP_ASSERT_CTX_INFO( context->info->recv_head.is_signed(), WJPErr_NotSigned, -1 );
+        _WJP_ASSERT_CTX_INFO( context->info->recv_head._dw3.sz >= 0, WJPErr_Size, -1 );
+
+        switch( context->info->recv_head._dw0.op ) {
             case WJPOp_Ping: {
-                _WJP_RR_ASSERT__SEND( 
-                    _send<>( nullptr, 0, WJPOp_Ack, info->recv_head._dw1.seq, 0, WJPSendMethod_Direct ), 
-                    WJPErr_Send 
-                );
+                _WJP_RR_ASSERT_AGENT( this->_resolve_ping( context ) );
             break; }
 
             case WJPOp_QGet: [[fallthrough]];
             case WJPOp_QSet: {
-                _WJP_RR_NAK_IF( info->recv_head._dw3.sz <= 0, "BAD_SIZE" );
-                char buffer[ info->recv_head._dw3.sz ];
-                _WJP_RR_ASSERT_RECV(
-                    _srwrap.recv( buffer, info->recv_head._dw3.sz, 0 ), info->recv_head._dw3.sz, WJPErr_Recv
-                );
-
-                char* delim = buffer - 1; while( *++delim != '\0' ) {
-                    _WJP_RR_NAK_IF( delim - buffer >= info->recv_head._dw3.sz - 1, "BAD_FORMAT" );
-                }
-
-                WJP_QGSTBL_ENTRY* entry = _qgstbl.query( buffer );
-                _WJP_RR_NAK_IF( entry == nullptr, "NO_ENTRY" );
-
-                if( info->recv_head._dw0.op == WJPOp_QSet ) goto l_set;
-            l_get:
-                if( entry->src != nullptr ) {
-                    _WJP_RR_ASSERT__SEND( 
-                        _send<>( entry->src, entry->sz, WJPOp_Ack, info->recv_head._dw1.seq, 0, WJPSendMethod_Direct ),
-                        WJPErr_Send
-                    );
-                } else {
-                    _WJP_RR_NAK_IF( true, "NO_PROCEDRE" );
-                }
-            break;
-            l_set:
-                _WJP_RR_NAK_IF( entry->read_only, "READ_ONLY" );
-
-                int32_t sz = info->recv_head._dw3.sz - ( delim - buffer + 1 );
-                _WJP_RR_NAK_IF( entry->sz != sz, "SIZE_MISMATCH" );
-
-                if( entry->qset_func != nullptr ) {
-                    if( const char* nakr = entry->qset_func( delim + 1, sz ); nakr != nullptr ) {
-                        info->nakr = nakr; goto l_gs_nak; 
-                    }
-                } else if( entry->src != nullptr ) {
-                    memcpy( entry->src, delim + 1, sz );
-                } else {
-                    _WJP_RR_NAK_IF( true, "NO_PROCEDRE" );
-                }
-                _WJP_RR_ASSERT__SEND( 
-                    _send<>( nullptr, 0, WJPOp_Ack, info->recv_head._dw1.seq, 0, WJPSendMethod_Direct ),
-                    WJPErr_Send
-                );
-            break; }
-
-            l_gs_nak: {
-                _WJP_RR_ASSERT__SEND(
-                    _send( info->nakr, info->nakr ? strlen( info->nakr ) + 1 : 0, WJPOp_Nak, info->recv_head._dw1.seq, 0, WJPSendMethod_Direct ),
-                    WJPErr_Send 
-                );
-
+                _WJP_RR_ASSERT_AGENT( this->_resolve_qgetset( context ) );
             break; }
 
             case WJPOp_Nak: [[fallthrough]];
             case WJPOp_Ack: {
-                _WJP_ASSERT_INFO( !_resolvers.empty(), WJPErr_NoResolver, -1 );
-
-                _WJP_RESOLVER& resolver = _resolvers.front();
-
-                _WJP_ASSERT_INFO( info->recv_head._dw1.seq == resolver._seq, WJPErr_Sequence, -1 );
-
-                WJP_WAIT_BACK_INFO* wb_info = resolver._info;
-
-                if( info->recv_head._dw0.op == WJPOp_Nak ) {
-                    //_WJP_ASSERT_INFO( info->recv_head._dw3.sz <= WJP_NAKR_MAX_SIZE, WJP_ERR_NAKR_SIZE, -1 );
-                    _WJP_RR_ASSERT_RECV(
-                        _srwrap.recv( wb_info->nakr, info->recv_head._dw3.sz, 0 ), info->recv_head._dw3.sz, WJPErr_Recv
-                    );
-                    goto l_resolver_sig;
-                }
-
-                if( resolver._dst == nullptr || info->recv_head._dw3.sz <= 0 ) goto l_resolver_sig;
-
-                _WJP_ASSERT_INFO( resolver._sz >= info->recv_head._dw3.sz, WJPErr_Size, -1 );
-                _WJP_RR_ASSERT_RECV(
-                    _srwrap.recv( resolver._dst, info->recv_head._dw3.sz, 0 ), info->recv_head._dw3.sz, WJPErr_Recv
-                );
-
-                wb_info->recv_count += info->recv_head._dw3.sz;
-
-            l_resolver_sig:
-                wb_info->recv_count += sizeof( WJP_HEAD );
-                
-                std::unique_lock< std::mutex > lock{ _resmtx };
-                _resolvers.pop_front();
-                lock.unlock();
-                
-                wb_info->resolved.store( true, std::memory_order_release );
-            #if defined( WJP_NOTIFIABLE_ATOMICS )
-                wb_info->resolved.notify_one();
-            #endif
+                _WJP_RR_ASSERT_AGENT( this->_resolve_acknak( context ) );
             break; }
 
             case WJPOp_IBurst: {
-                WJP_IBRSTBL_ENTRY* entry = _ibrstbl[ info->recv_head._dw1.seq ];
-                _WJP_ASSERT_INFO( entry != nullptr, WJPErr_NoEntry, -1 );
-                _WJP_ASSERT_INFO( info->recv_head._dw3.sz <= entry->sz, WJPErr_Size, -1 );
-
-                _WJP_RR_ASSERT_RECV(
-                    _srwrap.recv( entry->dst, info->recv_head._dw3.sz, 0 ), info->recv_head._dw3.sz, WJPErr_Recv
-                );
+                _WJP_RR_ASSERT_AGENT( this->_resolve_iburst( context ) );
             break; }
-
         }
-        
-        return sizeof( WJP_HEAD ) + info->recv_head._dw3.sz;
+
+        return context->recv_count;
     }
 
 #define _WJP_WB_ASSERT__SEND( s, e ) { int _sret = ( s ); if( _sret <= 0 ) { info->err = ( _sret == 0 ) ? WJPErr_ConnReset : e; return _sret; } }
